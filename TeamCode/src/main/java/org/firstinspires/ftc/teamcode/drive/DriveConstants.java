8f4dd31c4f56c1ba47df8ef7c4211f0638889084
DuchateOtaku578 <gabrielgaonas@outlook.com>
2023-12-06 12:29:01 -0600

    Las constantes ya quedaron tuneadas, ya estan de acuerdo a la grafica, vamos a ver que onda con los siguientes pasos

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -54,10 +54,10 @@ public class DriveConstants {
      * motor encoders or have elected not to use them for velocity control, these values should be
      * empirically tuned.
      */
-    public static double kV = 0.043;
+    public static double kV = 0.05;
 
-    public static double kA = 0.00999;
-    public static double kStatic = 0.055;
+    public static double kA = 0.0099;
+    public static double kStatic = 0.0458;
 
     /*
      * These values are used to generate the trajectories for you robot. To ensure proper operation,
